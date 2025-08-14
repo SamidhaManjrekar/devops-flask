59fb504de6196b022efcfb3909b70e544df6daed
SamidhaManjrekar <smanjrekar135@gmail.com>
2025-08-14 22:40:48 +0530

    Initial commit: Hello World Flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# We'll intentionally change this line differently on two branches later
+GREETING = "Hello, World!"
+
+@app.route("/")
+def hello():
+    # Just a simple log so you can see requests in the console
+    print("Request received at /")
+    return GREETING
+
+if __name__ == "__main__":
+    app.run(debug=True)
